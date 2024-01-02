57701d6d6b0eb4657e535699090168b9d073740e
Gevendra Sahu <123631888+gevendra2004@users.noreply.github.com>
2024-01-02 22:52:41 +0530

    Text-changing-animation

---
+++ Text-changing-animation/main.js
@@ -0,0 +1,39 @@
+var sliderCounter = 0;
+var sliderContent = [
+  "Web Development",
+  "ReactNative Development",
+  "App Development",
+  "UI/UX Designing",
+  "Web Designing"
+];
+var slider = document.querySelector("#slider");
+var sliderValue = document.querySelector("#sliderValue");
+
+function slide() {
+  if (sliderCounter >= sliderContent.length) {
+    sliderCounter = 0;
+  }
+
+  sliderValue.innerHTML = "";
+  
+  sliderValue.classList.remove("holder-animation");
+  sliderValue.classList.add("holder-animation");
+  
+  for (i = 0; i < sliderContent[sliderCounter].length; i++) {
+    let letterDiv = document.createElement("div");
+    letterDiv.innerHTML = sliderContent[sliderCounter][i];
+
+    if (letterDiv.innerHTML == " ") {
+      letterDiv.innerHTML = "&nbsp;";
+    }
+    letterDiv.classList.add("start");
+    letterDiv.classList.add("animation");
+    letterDiv.style.animationDelay = i / 10 + "s";
+    sliderValue.appendChild(letterDiv);
+  }
+
+  sliderCounter++;
+}
+
+slide();
+setInterval(slide, 4000);
